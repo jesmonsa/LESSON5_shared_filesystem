46c2c845875f232c9810c02b423a6f8ebcb46804
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-26 14:13:50 -0500

    Ultimo commit

--- compartment.tf
+++ compartment.tf
@@ -1,11 +1,11 @@
-resource "oci_identity_compartment" "Prod_01" {
-  provider       = oci.homeregion
-  name           = "Prod_01"
-  description    = "Prod_01 Compartment"
-  compartment_id = var.compartment_ocid
+resource "oci_identity_compartment" "Prod_01" { # definir el recurso del compartimento
+  provider       = oci.homeregion # definir el proveedor
+  name           = "Prod_01" # definir el nombre del compartimento
+  description    = "Prod_01 Compartment" # definir la descripción del compartimento
+  compartment_id = var.compartment_ocid # definir el OCID del compartimento padre
 
-  provisioner "local-exec" {
-    command = "sleep 60"
+  provisioner "local-exec" { # definir el provisioner
+    command = "sleep 60" # definir el comando a ejecutar en este caso un sleep de 60 segundos para esperar a que se cree el compartimento
   }
 }
 
