42cc0057ce51cd507a74ff9713e23ce65d9e89a9
Jesus Montoya <jesmonsa2018@gmail.com>
2024-07-06 14:20:22 -0500

    Ultimo commit

--- outputs.tf
+++ outputs.tf
@@ -8,13 +8,13 @@ output "LoadBalancer_Public_IP" {
   value = [for ip_detail in oci_load_balancer.LoadBalancer.ip_address_details : ip_detail.ip_address]
 }
 
-# WebServer1 Instance Public IP
-output "Webserver1PublicIP" { # definir la salida de la IP pública de la instancia
+# WebServer1 Instance Private IP
+output "Webserver1PrivateIP" { # definir la salida de la IP pública de la instancia
   value = [data.oci_core_vnic.Webserver1_VNIC1.private_ip_address] # definir la IP pública de la instancia
 }
 
-# WebServer2 Instance Public IP
-output "Webserver2PublicIP" { # definir la salida de la IP pública de la instancia
+# WebServer2 Instance Private IP
+output "Webserver2PrivateIP" { # definir la salida de la IP pública de la instancia
   value = [data.oci_core_vnic.Webserver2_VNIC1.private_ip_address] # definir la IP pública de la instancia
 }
 
