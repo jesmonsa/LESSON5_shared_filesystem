83bc32b1d63366281cd3e136558b2bcd2989ac71
Jesus Montoya <jesmonsa2018@gmail.com>
2024-07-07 16:17:59 -0500

    Ultimo commit

---
+++ fss.tf
@@ -0,0 +1,32 @@
+# Mount Target
+
+resource "oci_file_storage_mount_target" "MountTarget" {
+  availability_domain = var.availablity_domain_name == "" ? lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name") : var.availablity_domain_name
+  compartment_id      = oci_identity_compartment.Prod_01
+  subnet_id           = oci_core_subnet.WebSubnet.id
+  ip_address          = var.MountTargetIPAddress
+  display_name        = "MountTarget"
+}
+
+# Export Set
+
+resource "oci_file_storage_export_set" "Exportset" {
+  mount_target_id = oci_file_storage_mount_target.MountTarget.id
+  display_name    = "Exportset"
+}
+
+# FileSystem
+
+resource "oci_file_storage_file_system" "Filesystem" {
+  availability_domain = var.availablity_domain_name == "" ? lookup(data.oci_identity_availability_domains.ADs.availability_domains[0], "name") : var.availablity_domain_name
+  compartment_id      = oci_identity_compartment.Prod_01.id
+  display_name        = "Filesystem"
+}
+
+# Export
+
+resource "oci_file_storage_export" "FoggyKitchenExport" {
+  export_set_id  = oci_file_storage_mount_target.FoggyKitchenMountTarget.export_set_id
+  file_system_id = oci_file_storage_file_system.FoggyKitchenFilesystem.id
+  path           = "/sharedfs"
+}
